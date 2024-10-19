725a1faaa221f9f90d3c93bd2de80a8188f94b63
praneeth-coder-2005 <savediskofc@gmail.com>
2024-10-19 13:10:07 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-from flask import Flask, abort
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/link/<unique_id>', methods=['GET'])
-def confirm_link_access(unique_id):
-    """Show confirmation that the link is accessed."""
-    return f"<h1>Link with ID {unique_id} was accessed!</h1>"
+def confirm_access(unique_id):
+    """Display a confirmation message when a link is accessed."""
+    return f"<h1>Link with ID {unique_id} accessed successfully!</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
